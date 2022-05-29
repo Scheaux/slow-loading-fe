5d9123820892dd8a6e7f7b5dd2f15f298a86af6e
Scheaux <87568404+Scheaux@users.noreply.github.com>
2022-05-29 07:59:38 +0000

    Deploying to gh-pages from @ Scheaux/slow-loading-fe@1cac61ff749251d49e0fdd4bce767c014fc6f395 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/slow-loading-fe/sw.js', { scope: '/slow-loading-fe/' })})}
